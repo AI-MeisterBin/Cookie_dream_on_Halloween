0da974547cbb2275df7bbc13f69489e498540ac9
chokyeongbin <binyeong56@naver.com>
2021-11-16 00:44:40 +0900

    crash setting

--- main.py
+++ main.py
@@ -115,8 +115,45 @@ if __name__ == '__main__':
 
     #점수, 게임실행 코드
     score = 0
+    crash = True
     game_on = True
     while game_on:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_on = False
+
+            #충돌유무에 따른 화면 세팅
+            if crash:
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:     #게임시작 = crash False
+                    crash = False
+                    for i in range(pum_count):                                       #호박위치 설정
+                        pums[i].x = random.randrange(-150, -50)
+                        pums[i].y = random.randrange(0, win_h - pums[i].height)
+                        pums[i].load_image()
+
+                    player.y = win_h / 2                                             #위치 초기화
+                    player.dx = 0
+                    player.dy = 0
+                    score = 0
+                    pygame.mouse.set_visible(False)
+
+            if not crash:                                                            #플레이어가 움직임
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        player.dy = -7
+                    elif event.key == pygame.K_DOWN:
+                        player.dy = 7
+                    elif event.key == pygame.K_RIGHT:
+                        player.dx = 7
+                    elif event.key == pygame.K_LEFT:
+                        player.dx = -7
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        player.dy = 0
+                    elif event.key == pygame.K_DOWN:
+                        player.dy = 0
+                    elif event.key == pygame.K_RIGHT:
+                        player.dx = 0
+                    elif event.key == pygame.K_LEFT:
+                        player.dx = 0
